4c7774bd59c4e6d3a50c7320ab0e5bae3ec230cd
Rasmus Porsager <rasmus@porsager.com>
2021-10-14 12:57:46 +0200

    Old not working keyed

---
+++ keyed.js
@@ -0,0 +1,76 @@
+function Ref(keys, dom, key, i) {
+  keys[i] = { dom, key }
+  keys.rev[key] = i
+}
+
+export default function keyed(parent, as, bs, diff, remove, keys, after) { // eslint-disable-line
+  const map = as.rev
+
+  let ai = as.length - 1
+    , bi = bs.length - 1
+    , a = as[ai]
+    , b = bs[bi]
+    , temp = -1
+
+  outer: while (true) { // eslint-disable-line
+    while (a.key === b.key) {
+      after = diff(a.dom, b, parent).first
+      Ref(keys, after, b.key, bi)
+      delete map[b.key]
+
+      if (bi === 0)
+        break outer
+
+      if (ai === 0) {
+        b = bs[--bi]
+        break
+      }
+
+      a = as[--ai]
+      b = bs[--bi]
+    }
+
+    if (b.key in map) {
+      temp = map[b.key]
+      if (temp > bi) {
+        temp = diff(as[temp].dom, b, parent)
+        insertBefore(parent, temp, after)
+        after = temp.first
+        Ref(keys, after, b.key, bi)
+      } else if (temp !== bi) {
+        temp = diff(as[temp].dom, b, parent)
+        insertBefore(parent, temp, after)
+        after = temp.first
+        Ref(keys, after, b.key, bi)
+      } else {
+        a = as[--ai]
+        continue
+      }
+      delete map[b.key]
+      if (bi === 0)
+        break
+      b = bs[--bi]
+    } else {
+      temp = diff(null, b)
+      insertBefore(parent, temp, after)
+      after = temp.first
+      Ref(keys, after, b.key, bi)
+      if (bi === 0)
+        break
+      b = bs[--bi]
+    }
+  }
+
+  for (const k in map)
+    remove(as[map[k]].dom, parent)
+}
+
+function insertBefore(parent, { first, last }, before) {
+  let temp = first
+    , dom
+
+  do {
+    dom = temp
+    temp = dom.nextSibling
+  } while (parent.insertBefore(dom, before) !== last)
+}

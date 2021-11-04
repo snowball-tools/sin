bf8a455297d0c950f7f1b16faa59a0f959f16d90
Rasmus Porsager <rasmus@porsager.com>
2021-11-04 21:55:58 +0100

    play around

--- play.js
+++ play.js
@@ -1,70 +1,101 @@
 import s from './index.js'
+import html from './html.js'
 
-window.run = s.redraw
+//window.run = s.redraw
 
-const nested = s(async({ route, id }) => {
-  await new Promise(r => setTimeout(r, 500))
-  const items = [
-    ...Array(
-      5 + Math.ceil(Math.random() * 5)
-    ).keys()
-  ]
+s.css`
+  .dragging * {
+    user-select: none;
+    cursor: grabbing;
+  }
 
-  return () => s`
-    d flex
-  `(
-    s`nav
-      w 100
-      mr 4m
-    `(
-      items.map(x =>
-        s`a
-          p 4
-          c black
-          display block
-          [selected] {
-            bc hsl(200, 100%, 50%)
-            c white
-          }
-        `({
-          selected: route.has(x),
-          href: route + x
-        }, x)
-      )
-    ),
-    p(route({
-      '/:id': nested
-    }), 'VIEW')
-  )
-}, 'Loading')
+   @font-face {
+    font-family: ptroot;
+    src: url('/fonts/PT-Root-UI_Light.woff2') format('woff2'),
+        url('/fonts/PT-Root-UI_Light.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
 
-s.mount(({ route }) =>
-  s`main
-    ff sans-serif
-  `(
-    s`a
-      d block
-      p 8
-    `({
-      href: '/4/1'///2/3/0/4/2'
-    }, 'long test'),
-    s`
-      p 6 16
-      br 20
-      bc #eee
-      mb 16
-    `(
-      location.pathname.split('/').join(' / ')
-    ),
-    nested({ route })
-  )
+  @font-face {
+    font-family: ptroot;
+    src: url('/fonts/PT-Root-UI_Medium.woff2') format('woff2'),
+      url('/fonts/PT-Root-UI_Medium.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+  }
+`
+const app = () => s`div`(
+  s`h1`('hej'),
+  s`h2`('dig')
 )
 
+p(await s.html(app()))
+p(s.style.sheet.cssRules)
+
+s.mount(app)
 
 
 
+function nestedRouting() {
+  const nested = s(async({ route, id }) => {
+    await new Promise(r => setTimeout(r, 500))
+    const items = [
+      ...Array(
+        5 + Math.ceil(Math.random() * 5)
+      ).keys()
+    ]
 
+    return () => s`
+      d flex
+    `(
+      s`nav
+        w 100
+        mr 4m
+      `(
+        items.map(x =>
+          s`a
+            p 4
+            c black
+            display block
+            [selected] {
+              bc hsl(200, 100%, 50%)
+              c white
+            }
+          `({
+            selected: route.has(x),
+            href: route + x
+          }, x)
+        )
+      ),
+      route({
+        '/:id': nested
+      })
+    )
+  }, 'Loading')
 
+  s.mount(({ route }) =>
+    s`main
+      ff sans-serif
+    `(
+      s`a
+        d block
+        p 8
+      `({
+        href: '/4/1//2/3/0/4/2'
+      }, 'long test'),
+      s`
+        p 6 16
+        br 20
+        bc #eee
+        mb 16
+      `(
+        location.pathname.split('/').join(' / ')
+      ),
+      nested({ route })
+    )
+  )
+}
 
 function modals() {
   const modals = []
@@ -243,7 +274,7 @@ function udomdiffTest() {
 
   let current = 0
   s.mount(() => [
-    s`button`({ onclick: start }),
+    s`button`({ onclick: start }, ''),
     xs[current].split('').map(key => s`div`({ key, id: key }, key))
   ])
 

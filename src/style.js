20d5ca4166acea54f491f5f403c7bba3bb9bf551
Rasmus Porsager <rasmus@porsager.com>
2022-08-27 00:53:54 +0200

    Fix multiple font-face

--- src/style.js
+++ src/style.js
@@ -305,7 +305,7 @@ function startBlock(i) {
   } else {
     rule && (rules[path || '&'] = rule)
     selector = startChar === 64 // @
-      ? atHelper(prop + (value || ' ') + x.slice(valueStart, i).trim())
+      ? atHelper(prop + (value || ' ') + x.slice(valueStart, i)).trim()
       : x.slice(start, i).trim()
     selector.indexOf(',') !== -1 && (selector = splitSelector(selector))
     value = prop = ''

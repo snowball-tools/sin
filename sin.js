48eb158eee6cb63508438b886a4a161a3d1f692d
Rasmus Porsager <rasmus@porsager.com>
2022-09-02 13:08:01 +0200

    build

--- sin.js
+++ sin.js
@@ -507,7 +507,7 @@ function startBlock(i) {
     rule = "";
   } else {
     rule && (rules[path || "&"] = rule);
-    selector = startChar === 64 ? atHelper(prop) + (value || " ") + x.slice(valueStart, i) : x.slice(start, i).trim();
+    selector = (startChar === 64 ? atHelper(prop) + (value || " ") + x.slice(valueStart, i) : x.slice(start, i)).trim();
     selector.indexOf(",") !== -1 && (selector = splitSelector(selector));
     value = prop = "";
     selectors.push(

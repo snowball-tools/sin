488f40bae6e3c95ee23f22a1f1816d14da2ada54
Rasmus Porsager <rasmus@porsager.com>
2022-10-28 12:20:21 +0200

    build

--- sin.js
+++ sin.js
@@ -404,11 +404,12 @@ function parse([xs, ...args], parent, nesting = 0, root) {
         temp = prefix + Math.abs(hash).toString(31);
         vars[varName = "--" + temp + j] = { property: prop, unit: getUnit(prop, last(fn)), index: j };
         value += before + "var(" + varName + ")";
+        valueStart = 0;
       } else {
         args[j] && (x = args[j] + x);
         cacheable = false;
+        valueStart = -1;
       }
-      valueStart = 0;
     }
   }
   if (hasRules) {
@@ -476,7 +477,7 @@ function parseStyles(idx, end) {
   for (let i = idx; i <= x.length; i++) {
     char = x.charCodeAt(i);
     i < x.length && (hash = Math.imul(31, hash) + char | 0);
-    if (quote === -1 && valueStart >= 0 && (colon ? strict(char) : valueEndChar(char) || end && i === x.length))
+    if (quote === -1 && valueStart !== -1 && (colon ? strict(char) : valueEndChar(char) || end && i === x.length))
       addRule(i);
     if (quote !== -1) {
       if (quote === char && x.charCodeAt(i - 1) !== 92)

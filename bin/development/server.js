f95bca9d8a71480cee47b54b7cff80ed8f61cecb
Rasmus Porsager <rasmus@porsager.com>
2023-01-04 17:19:41 +0100

    Add reset command

--- bin/development/server.js
+++ bin/development/server.js
@@ -42,6 +42,20 @@ const env = process.env
     , sockets = new Set()
     , sinRoot = path.join(Url.fileURLToPath(new URL('.', import.meta.url)), '..', '..')
 
+
+if (command === 'reset') {
+  const xs = argv[1] === 'all'
+    ? fs.readdirSync(home).filter(x => fs.statSync(path.join(home, x)).isDirectory())
+    : [name]
+
+  for (const x of xs) {
+    process.stdout.write('Clear ' + x + ' ' + s.gray('(' + path.join(home, x) + ')') + ' ... ')
+    await fsp.rm(path.join(home, x), { recursive: true, force: true }).catch(() => {})
+    console.log('done')
+  }
+  process.exit(0)
+}
+
 Object.values(scripts).forEach(watchAdd)
 
 fs.mkdirSync(home, { recursive: true })

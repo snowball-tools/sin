40e24062993f76cca8d479bdfde4036245964c00
Rasmus Porsager <rasmus@porsager.com>
2023-03-28 13:55:09 +0200

    Add --workers support for production

--- bin/production/index.js
+++ bin/production/index.js
@@ -7,9 +7,17 @@ const argv = process.argv.slice(2)
 const entry = argv.find(x => x.startsWith('./') || x.endsWith('.js')) || ''
 const root = path.isAbsolute(entry) ? entry : path.join(process.cwd(), entry)
 const file = root.endsWith('.js')
+const workers = parseInt(argv.find((x, i) => argv[i - 1] === '--workers') || 1)
 
 process.chdir(process.env.PWD = file ? path.dirname(root) : root)
 
-argv[0] === 'raw'
-  ? import(file ? root : path.join(root, 'index.js'))
-  : import('./server.js')
+const url = argv[0] === 'raw'
+  ? file ? root : path.join(root, 'index.js')
+  : './server.js'
+
+if (workers > 1) {
+  for (let i = 0; i < workers; i++)
+    new Worker(new URL(url, import.meta.url), { argv })
+} else {
+  import(url)
+}

1a5ae6d74954da084959aba88ab8b60c58c858ae
Rasmus Porsager <rasmus@porsager.com>
2023-03-28 22:17:47 +0200

    Fix setSelectionRange throw on not supported input types

--- src/index.js
+++ src/index.js
@@ -782,9 +782,11 @@ function attributes(dom, view, context) {
       attr === 'deferrable' && (dom[deferrableSymbol] = view.attrs[attr])
     } else if (attr === 'value' && tag.name === 'input' && dom.value !== '' + view.attrs[attr]) {
       value = view.attrs[attr]
-      const before = [dom.selectionStart, dom.selectionEnd]
+      const start = dom.selectionStart
+      const end = dom.selectionEnd
       updateAttribute(dom, context, view.attrs, attr, dom.value, value, create)
-      dom.setSelectionRange(...before)
+      if (dom.selectionStart !== start || dom.selectionEnd !== end)
+        dom.setSelectionRange(start, end)
     } else if (!prev || prev[attr] !== view.attrs[attr]) {
       value = view.attrs[attr]
       updateAttribute(dom, context, view.attrs, attr, prev && prev[attr], value, create)

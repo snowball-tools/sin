20890a91bdc6521b308a64b5a07daffe3f65d673
Rasmus Porsager <rasmus@porsager.com>
2023-11-06 11:54:51 +0100

    Remove old examples

--- examples/deno/index.js
+++
@@ -1,3 +0,0 @@
-import s from '../../src/index.js'
-
-export default s.mount(() => s`h1`('Hello World'))
--- examples/deno/server.js
+++
@@ -1,5 +0,0 @@
-import { serve } from "https://deno.land/std@0.153.0/http/server.ts"
-import ssr from '../../ssr/deno.js'
-import app from './index.js'
-
-await serve(ssr(app), { port: 8000 })

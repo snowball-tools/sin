d3a3297013e06f4b2a48f6a16e35c4e774b4c574
Rasmus Porsager <rasmus@porsager.com>
2024-01-12 23:14:00 +0100

    hasColors is not a function

--- bin/color.js
+++ bin/color.js
@@ -1,9 +1,8 @@
 import util from 'util'
 
-const hasColors = process.stdout.hasColors()
 const style = Object.entries(util.inspect.colors).reduce((acc, [name, [start, end]]) => {
   acc[name] = (xs, ...args) => {
-    if (!hasColors || process.env.NO_COLOR)
+    if (!process.stdout.hasColors || process.env.NO_COLOR)
       return xs.raw ? String.raw(xs, ...args) : xs
 
     return [].concat(xs).map((x, i) =>

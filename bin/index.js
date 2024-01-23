225d33b124208496cd991a91c6605f2be66adb32
Rasmus Porsager <rasmus@porsager.com>
2024-01-23 09:11:13 +0100

    Fix being run as sub command (Force enable of Websocket)

--- bin/index.js
+++ bin/index.js
@@ -4,10 +4,13 @@ import path from 'path'
 
 import config from './config.js'
 import c from './color.js'
+import cp from 'child_process'
 
-config.command
-  ? import(path.join(config.local, 'bin', config.command, 'index.js'))
-  : notFound()
+process.execArgv.includes('--experimental-websocket')
+  ? config.command
+    ? import(path.join(config.local, 'bin', config.command, 'index.js'))
+    : notFound()
+  : cp.execFileSync(process.argv[0], ['--experimental-websocket', '--no-warnings', ...process.argv.slice(1)], { stdio: 'inherit' })
 
 function notFound() {
   console.log( // eslint-disable-line

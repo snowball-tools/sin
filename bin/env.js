e66c87566561a337053d863932974d50019b5364
Gilbert <gilbertbgarza@gmail.com>
2024-07-03 11:18:37 -0500

    Feature: Read and/or redirect specified env file

--- bin/env.js
+++ bin/env.js
@@ -1,9 +1,9 @@
 import path from 'node:path'
 import fs from 'node:fs'
 
-Object.assign(process.env, load())
+Object.assign(process.env, load(process.env.SIN_ENV))
 
-export default function load(env = path.join(process.cwd(), '.env')) {
+export default function load(env = path.join(process.cwd(), '.env'), depth=0) {
   const xs = {}
   try {
     fs.readFileSync(env, 'utf8').split('\n').forEach((x, i) => (
@@ -13,6 +13,10 @@ export default function load(env = path.join(process.cwd(), '.env')) {
   } catch (err) {
     if (err.code !== 'ENOENT')
       throw err
+  }
+  if (xs.SIN_ENV && depth === 0) {
+    console.log("Loading SIN_ENV", xs.SIN_ENV)
+    return load(xs.SIN_ENV, depth + 1)
   }
   return xs
 }

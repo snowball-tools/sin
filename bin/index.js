b6f3011d4372d2927de81b570a90e98cf5220803
Rasmus Porsager <rasmus@porsager.com>
2024-10-01 21:35:17 +0200

    Properly exit with exit code on error

--- bin/index.js
+++ bin/index.js
@@ -13,17 +13,18 @@ if (major < 20 || major === 20 && minor <= 10)
   throw error('Sins minimum Node.js support is 20.11, please upgrade')
 
 try {
-  if (major >= 22 || process.execArgv.includes('--experimental-websocket'))
-    await import(URL.pathToFileURL(path.join(config.local, 'bin', config.$[0], 'index.js')))
-  else {
+  if (config.develop && major < 22 && !process.execArgv.includes('--experimental-websocket')) {
     const x = cp.spawnSync(process.argv[0], ['--experimental-websocket', '--no-warnings', ...process.argv.slice(1)], { stdio: 'inherit' })
     process.exitCode = x.status
+  } else {
+    await import(URL.pathToFileURL(path.join(config.local, 'bin', config.$[0], 'index.js')))
   }
   config.config && console.log(config) // eslint-disable-line
+  prexit.exit()
 } catch (e) {
   config.config && console.log(config) // eslint-disable-line
-  config.debug && console.error(e)
-  error(e) // eslint-disable-line
+  config.debug
+    ? console.error(e)
+    : error(e) // eslint-disable-line
+  prexit.exit(1)
 }
-
-prexit.exit()

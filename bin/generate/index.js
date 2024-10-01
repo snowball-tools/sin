8cb958e28057a088c35d242de55aa74ef0a1e53b
Rasmus Porsager <rasmus@porsager.com>
2024-10-01 22:05:41 +0200

    Use spawnSync instead of fork for simpler process finish

--- bin/generate/index.js
+++ bin/generate/index.js
@@ -1,13 +1,13 @@
 import cp from 'node:child_process'
 import url from 'node:url'
 
-cp.fork(
-  url.fileURLToPath(new URL('generate.js', import.meta.url)),
-  process.argv.slice(2),
-  {
-    execArgv: [
-      '--import',
-      url.fileURLToPath(new URL('../start/import.js', import.meta.url))
-    ]
+cp.spawnSync(
+  process.execPath, [
+    '--import',
+    url.fileURLToPath(new URL('../start/import.js', import.meta.url)),
+    url.fileURLToPath(new URL('generate.js', import.meta.url)),
+    ...process.argv.slice(2)
+  ], {
+    stdio: 'inherit'
   }
 )

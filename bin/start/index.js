289224ad4ebe4479533629021792e9e42d6314e8
Rasmus Porsager <rasmus@porsager.com>
2024-10-01 22:22:49 +0200

    Fix sin start

--- bin/start/index.js
+++ bin/start/index.js
@@ -3,13 +3,13 @@ import url from 'node:url'
 
 process.env.NODE_ENV = 'production'
 
-cp.fork(
-  url.fileURLToPath(new URL('node.js', import.meta.url)),
-  process.argv.slice(2),
-  {
-    execArgv: [
-      '--import',
-      url.fileURLToPath(new URL('import.js', import.meta.url))
-    ]
+cp.spawnSync(
+  process.execPath, [
+    '--import',
+      url.fileURLToPath(new URL('import.js', import.meta.url)),
+    url.fileURLToPath(new URL('node.js', import.meta.url)),
+    process.argv.slice(2)
+  ], {
+    stdio: 'inherit'
   }
 )

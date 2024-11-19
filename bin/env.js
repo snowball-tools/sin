65eedcd088be1c558c0be47c3beaaf43acf01bc9
Rasmus Porsager <rasmus@porsager.com>
2024-11-19 14:42:19 +0100

    .env load from parent folders too

--- bin/env.js
+++ bin/env.js
@@ -2,18 +2,35 @@ import fs from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
 
-Object.assign(process.env, load())
+Object.assign(process.env, load(path.join(process.cwd(), '.env'), true))
 
-export default function load(env = path.join(process.cwd(), '.env')) {
+export default function load(x = path.join(process.cwd(), '.env'), parents = false) {
   const xs = {}
-  try {
-    fs.readFileSync(env, 'utf8').split('\n').forEach((x, i) => (
-      x = x.trim(), i = x.indexOf('='), x && i > 0 && x[0] !== '#' &&
-      (xs[x.slice(0, i)] = x.slice(i + 1))
-    ))
-  } catch (err) {
-    if (err.code !== 'ENOENT')
-      throw err
+  const filename = path.basename(x)
+  let dir = path.dirname(x)
+  let prev
+  while (dir !== prev) {
+    try {
+      fs.readFileSync(path.join(dir, filename), 'utf8').split('\n').forEach((x, i) => {
+        x = x.trim()
+        if (x[0] === '#')
+          return
+
+        i = x.indexOf('=')
+        if (i < 1)
+          return
+
+        const env = x.slice(0, i)
+            , value = x.slice(i + 1)
+
+        env in xs || (xs[env] = value)
+      })
+    } catch (err) {
+      if (err.code !== 'ENOENT')
+        throw err
+    }
+    prev = dir
+    parents && (dir = path.dirname(dir))
   }
   return xs
 }
